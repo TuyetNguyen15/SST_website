13eb647095a56ca292be37ee69bacdcb20f70362
Lignna <nganlinhh60@gmail.com>
2025-06-16 10:52:04 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./TailwindApp.js",
+"./components/*.js",
+"./screens/*.js"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
