4bea306812011407c1c8cb85274a4139dfcb0e8c
NguyenTuyet <Yang2004tuyet@gmail.com>
2025-06-26 11:27:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Home from './pages/home';
 import AIPage from './pages/AISolution';
 import BlockchainServices from "./pages/BlockchainServices";
-import GameService from "./pages/GameService"
- function App() {
+import GameService from "./pages/GameService";
+
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+
+    if (!location.hash) {
+      window.scrollTo(0, 0);  
+    }
+  }, [location.pathname]);
+
+  return null;
+}
+
+function App() {
   return (
     <Router>
       <div className="app">
-       
+        <ScrollToTop /> 
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/services/ai-solution' element={<AIPage/>}></Route>
-          <Route path='/services/blockchain-solution' element={<BlockchainServices/>}></Route>
-          <Route path='/services/game-service' element={<GameService/>}></Route>
-       
+          <Route path='/' element={<Home />} />
+          <Route path='/services/ai-solution' element={<AIPage />} />
+          <Route path='/services/blockchain-solution' element={<BlockchainServices />} />
+          <Route path='/services/game-service' element={<GameService />} />
         </Routes>
       </div>
     </Router>
   );
 }
+
 export default App;
-
-
-
-
-
-
